a171b6b9d4cc5a635992c32c4af303fb8c620ed8
Parasbhardwaj <33519749+Parasbhardwaj@users.noreply.github.com>
2021-05-26 13:41:49 +0530

    Conditional statements and looping and v-model example

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div></div>
+    <!-- <input type="text" v-model="message"> -->
+    <HelloWorld msg="paras" />
+    <div v-if="num == 4">Paras</div>
+    <div v-else-if="num == 55">Prince</div>
+    <div v-else>Tamanna</div>
+    <div v-for="index in names" v-bind:key="index.id">
+      {{ index.id }} {{ index.name }}
+    </div>
+    <button v-on:click="onclicked">click here</button>
+    <h1>{{ newData }}</h1>
+    <input type="text" v-model="newData">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  created() {
+    this.sayHello("created hello");
+    this.num = 55;
+  },
+  mounted() {
+    this.sayHello("mounted hello");
+    this.num = 66;
+  },
+  data() {
+    return {
+      num: 4,
+      names: [
+        {
+          id: 1,
+          name: "paras",
+        },
+        {
+          id: 2,
+          name: "tamanna",
+        },
+        {
+          id: 3,
+          name: "prince",
+        },
+      ],
+      newData : "Hello",
+    };
+  },
+
+  methods: {
+    sayHello(greetings) {
+      console.log(greetings);
+      return greetings;
+    },
+    onclicked() {
+      console.log("Paras Bhardwaj");
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Class.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            {{ num }}
+        </p>
+    </div>
+</template>
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+    name : "class3",
+    components : {
+        
+    }
+}
+</script>
